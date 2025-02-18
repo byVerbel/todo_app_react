c0cf345933652aa886b4392c927da0286575f39d
byverbel <byverbel@gmail.com>
2025-02-18 05:05:19 -0500

    fix error where checkboxes remained the same

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,29 +1,33 @@
 import { RiDeleteBin5Fill } from "react-icons/ri";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const ToDo = ({id, name, completed, updateCompletedToDo, deleteToDo}) => {
 
     const [isChecked, setIsChecked] = useState(completed);
 
+    useEffect(() => {
+        setIsChecked(completed);
+    }, [completed]);
+
     const handleCompleted = async () => {
         await updateCompletedToDo(id, !isChecked);
         setIsChecked(!isChecked);
-    }
+    };
 
     const handleDelete = async () => {
         await deleteToDo(id);
-    }
+    };
 
     return (
         <div className="to-do">
             <div className="to-do-container">
                 <input type="checkbox" checked={isChecked} onChange={handleCompleted} />
                 <h3>{name}</h3>
-                <RiDeleteBin5Fill onClick={handleDelete} size='20px'/>
+                <RiDeleteBin5Fill onClick={handleDelete} size="20px" />
             </div>
         </div>
     );
-}
+};
 
 export default ToDo;
