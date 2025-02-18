8a36fc094bd9619aea0482a263955394393b4c13
byverbel <byverbel@gmail.com>
2025-02-18 03:51:42 -0500

    adds post_to_do endpoint

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -2,10 +2,14 @@ import axios from 'axios';
 
 const BASE_URL = 'http://localhost:3000/api';
 const GET_URL = `${BASE_URL}/to_dos`;
+const POST_URL = `${BASE_URL}/to_dos`;
 
-const get_to_dos = async () => {
+export const get_to_dos = async () => {
     const response = await axios.get(GET_URL);
     return response.data;
 }
 
-export { get_to_dos };
+export const post_to_do = async (name) => {
+    const response = await axios.post(POST_URL, { 'name': name, 'completed': false });
+    return response.data;
+}
