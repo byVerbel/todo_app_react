f15f8685c07bfde80da375cc4f5faaf46a5860b1
byverbel <byverbel@gmail.com>
2025-02-18 04:29:28 -0500

    adds delete_to_do endpoint

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 const BASE_URL = 'http://localhost:3000/api';
 const GET_URL = `${BASE_URL}/to_dos`;
 const POST_URL = `${BASE_URL}/to_dos`;
+const DELETE_URL = (id) => `${BASE_URL}/to_dos/${id}`;
 
 export const get_to_dos = async () => {
     const response = await axios.get(GET_URL);
@@ -11,5 +12,10 @@ export const get_to_dos = async () => {
 
 export const post_to_do = async (name) => {
     const response = await axios.post(POST_URL, { 'name': name, 'completed': false });
+    return response.data;
+}
+
+export const delete_to_do = async (id) => {
+    const response = await axios.delete(DELETE_URL(id));
     return response.data;
 }
