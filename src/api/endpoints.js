a5556deb5758b460e8b3fd99f539eecce49446df
byverbel <byverbel@gmail.com>
2025-02-18 04:51:36 -0500

    adds update_completed_to_do endpoint

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 const BASE_URL = 'http://localhost:3000/api';
 const GET_URL = `${BASE_URL}/to_dos`;
 const POST_URL = `${BASE_URL}/to_dos`;
+const UPDATE_COMPLETED_URL = (id) => `${BASE_URL}/to_dos/${id}/update_completed`;
 const DELETE_URL = (id) => `${BASE_URL}/to_dos/${id}`;
 
 export const get_to_dos = async () => {
@@ -12,6 +13,11 @@ export const get_to_dos = async () => {
 
 export const post_to_do = async (name) => {
     const response = await axios.post(POST_URL, { 'name': name, 'completed': false });
+    return response.data;
+}
+
+export const update_completed_to_do = async (id, completed) => {
+    const response = await axios.patch(UPDATE_COMPLETED_URL(id), { 'completed': completed });
     return response.data;
 }
 
